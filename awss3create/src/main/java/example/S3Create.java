4774cd4a33c77532cc2246adfb7ee85ef153cdde
alfonsof <alfonsofb@gmail.com>
2021-03-10 23:26:47 +0100

    First commit

---
+++ awss3create/src/main/java/example/S3Create.java
@@ -0,0 +1,93 @@
+/**
+ * S3Create is an example that handles S3 buckets on AWS.
+ * Create a new S3 bucket.
+ * You must provide 1 parameter:
+ * BUCKET_NAME = Name of the bucket
+ */
+
+package example;
+
+import java.io.IOException;
+import software.amazon.awssdk.core.exception.SdkException;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import software.amazon.awssdk.core.waiters.WaiterResponse;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
+import software.amazon.awssdk.services.s3.model.HeadBucketResponse;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.GetBucketLocationRequest;
+import software.amazon.awssdk.services.s3.model.GetBucketLocationResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.waiters.S3Waiter;
+
+public class S3Create {
+
+    private static final Region REGION = Region.of("eu-west-1");      // Region name
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length < 1) {
+            System.out.println("Not enough parameters.\nProper Usage is: java -jar s3create.jar <BUCKET_NAME>");
+            System.exit(1);
+        }
+
+        // The name for the new bucket
+        String bucketName = args[0];
+
+        System.out.println("Bucket name: " + bucketName);
+
+        // Instantiates a client
+        S3Client s3client = S3Client.builder()
+                .region(REGION)
+                .build();
+
+        try {
+            System.out.println("Creating bucket ...");
+            S3Waiter s3Waiter = s3client.waiter();
+            CreateBucketRequest bucketRequest = CreateBucketRequest.builder()
+                    .bucket(bucketName)
+                    .build();
+
+            s3client.createBucket(bucketRequest);
+            HeadBucketRequest bucketRequestWait = HeadBucketRequest.builder()
+                    .bucket(bucketName)
+                    .build();
+
+            // Wait until the bucket is created and print out the response.
+            WaiterResponse<HeadBucketResponse> waiterResponse = s3Waiter.waitUntilBucketExists(bucketRequestWait);
+            waiterResponse.matched().response().ifPresent(System.out::println);
+            System.out.println("Created");
+
+            // Get Bucket location
+            GetBucketLocationRequest bucketLocationRequest = GetBucketLocationRequest.builder().bucket(bucketName).build();
+            GetBucketLocationResponse bucketLocationResponse = s3client.getBucketLocation(bucketLocationRequest);
+            System.out.println("Bucket location: " + bucketLocationResponse.locationConstraintAsString());
+        } catch (S3Exception e) {
+            if (e.statusCode() == 409) {
+                System.out.println("Error: Bucket already exists!!");
+            } else {
+                System.out.println("HTTP Status Code:  " + e.statusCode());
+                System.out.println("S3Exception: " + e);
+            }
+        } catch (AwsServiceException ase) {
+            System.out.println("Caught an AwsServiceException, " +
+                    "which means your request made it " +
+                    "to AWS S3, but was rejected with an error " +
+                    "response for some reason.");
+            System.out.println("Error Message:     " + ase.getMessage());
+            System.out.println("HTTP Status Code:  " + ase.statusCode());
+            System.out.println("AWS Service Name:  " + ase.awsErrorDetails().serviceName());
+            System.out.println("AWS Error Code:    " + ase.awsErrorDetails().errorCode());
+            System.out.println("AWS Error Message: " + ase.awsErrorDetails().errorMessage());
+            System.out.println("Request ID:        " + ase.requestId());
+        } catch (SdkException se) {
+            System.out.println("Caught an SdkException, " +
+                    "which means the client encountered " +
+                    "an internal error while trying to " +
+                    " communicate with AWS S3, " +
+                    "such as not being able to access the network.");
+            System.out.println("Error Message: " + se.getMessage());
+        }
+    }
+}
